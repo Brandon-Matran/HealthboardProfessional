87a8f6dda6639f94e1843cbfd61fda4024d8ba16
Brandon Matran <brandon.matran@gmail.com>
2023-05-12 10:15:48 -0700

    Get endpoint for staff

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,39 +1,30 @@
-const { Sequelize} = require('sequelize');
-const sequelize = new Sequelize("postgres://root:root@postgres:5432/root")
-const { Patient_User } = require('../tables/userTables')
-const express = require('express')
-const router = express.Router()
+const { Sequelize } = require("sequelize");
+const sequelize = new Sequelize("postgres://root:root@postgres:5432/root");
+const { Patient_User, Healthcare_Provider } = require("../tables/userTables");
+const express = require("express");
+const router = express.Router();
 
 router.get("/patients", async (req, res) => {
-    try {
+  try {
     const users = await Patient_User.findAll({
-        attributes: ['id', 'firstName', 'lastName']
-    })
-    console.log(res.json(users))
-    return res.json(users)
-}
-    catch (err) {
-        console.log(err)
-    }
-})
+      attributes: ["id", "firstName", "lastName"],
+    });
+    console.log(res.json(users));
+    return res.json(users);
+  } catch (err) {
+    console.log(err);
+  }
+});
 
-router.get("staff", async (req, res) => {
-    try{
-        const staff = await 
-    })
+router.get("/staff", async (req, res) => {
+  try {
+    const staff = await Healthcare_Provider.findAll({
+      attributes: ["id", "firstName", "lastName", "email", "role"],
+    });
+    return res.json(staff);
+  } catch (err) {
+    console.log(err);
+  }
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-module.exports = router
+module.exports = router;

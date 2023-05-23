158dc7a2d62791cbc3242927b9f5a427a5e1e96f
Brandon Matran <brandon.matran@gmail.com>
2023-05-23 11:28:37 -0700

    Delete and Put Endpoint for medication route complete

--- server/routes/medications.js
+++ server/routes/medications.js
@@ -31,6 +31,20 @@ router.get("/medications/:patientId", async (req, res) => {
   }
 });
 
+router.get("/medications/:patientId/:id", async (req, res) => {
+  try {
+    const medication = await medicationTable.findOne({
+      where: {
+        patientId: req.params.patientId,
+        id: req.params.id,
+      },
+    });
+    return res.json(medication);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
 router.post("/medications/:patientId", async (req, res) => {
   try {
     const medication = await medicationTable.create({
@@ -52,4 +66,40 @@ router.post("/medications/:patientId", async (req, res) => {
     console.log(err);
   }
 });
+
+router.put("/medications/:patientId/:id", async (req, res) => {
+  try {
+    const medication = await medicationTable.findOne({
+      where: { patientId: req.params.patientId, id: req.params.id },
+    });
+    await medication.update({
+      patientId: req.body.patientId,
+      medication: req.body.medication,
+      dosage: req.body.dosage,
+      dosageType: req.body.dosageType,
+      route: req.body.route,
+      start: req.body.start,
+      end: req.body.end,
+      frequency: req.body.frequency,
+    });
+    return res.json(medication)
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.delete("/medications/:patientId/:id", async (req, res) => {
+    try{
+        const medication = await medicationTable.findOne({
+            where: { patientId: req.params.patientId, id: req.params.id },
+          });
+          if (medication) {
+            medication.destroy();
+            return res.json(`${medication.medication} deleted for user ${medication.patientId}`);
+          }
+          else if (medication === null || medication === undefined) {
+            return res.json("The medication either does not exist or is not available for this user")
+    }} catch(err){
+        console.log(err);
+    }})
 module.exports = router;
